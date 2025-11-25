a8cfbd2af8fc4d3d94cc04f27ac625c63a27da62
Oseyemi Damilola <osedee101@gmail.com>
2025-11-25 13:39:12 +0100

    Added swiper.js for the carousel images

--- damsel.js
+++ damsel.js
@@ -4,3 +4,16 @@ const mobileMenu = document.getElementById("mobile-menu");
 hamburger.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
